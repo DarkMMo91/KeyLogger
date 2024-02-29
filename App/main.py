6bd4cc00b8930b6eaf42aacbc9b03191680009af
Mark <soldano9carlo@gmail.com>
2024-02-29 22:47:14 +0100

    Date: 29/02/2024

--- App/main.py
+++ App/main.py
@@ -4,19 +4,19 @@ from pynput import *
 def on_press(key):
     print("Hai premuto il tasto: " + str(key))
 
+    #if statement per fermare il programma quando si preme esc o invio
+    if key == keyboard.Key.esc or key == keyboard.Key.enter:
+        return False                            
     
     #2 linee di codice per inserire le chiavi digitate in un file .txt chiamato Log
     file = open("Log.txt", "a")
 
-    file.write(str(key))
-    
-    #if statement per fermare il programma quando si preme esc o invio
-    if key == keyboard.Key.esc or key == keyboard.Key.enter:
-        return False                            
+    file.write(str(key) + "\n")
+    file.close()
 
 #Funzione per quando si rilascia un tasto sulla tastiera
 def on_release(key):
-    pass
+    print("Hai rilasciato il tasto: " + str(key))
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
